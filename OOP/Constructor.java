4db512319bee5ff5ff9e2820ae1961fcca9c5732
yashrbankar <yashrbankar@gmail.com>
2022-11-21 23:42:48 +0530

    constructor

---
+++ OOP/Constructor.java
@@ -0,0 +1,32 @@
+class Constructor
+{
+    public static void main(String args[])
+    {
+        Dog b=new Dog();
+        
+    }
+};
+
+
+class animal
+{
+     animal()
+     {
+          System.out.println("animal default");
+     }
+     animal(int val)
+     {
+          System.out.println("animal : default ");
+     }
+};
+
+class Dog extends
+{
+    String color;
+    Dog()
+    {
+       System.out.println("dog default");
+    }
+}
+
+
